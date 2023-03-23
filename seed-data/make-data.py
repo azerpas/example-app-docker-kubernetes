93cca25ee130efa3b43b39d0ca6036d2c71cf63f
Azerpas <manikhouth.anthony@gmail.com>
2023-03-23 11:55:33 +0100

    fix: seed-data format

--- seed-data/make-data.py
+++ seed-data/make-data.py
@@ -1,13 +1,14 @@
 # this creates urlencode-friendly files without EOL
 import urllib.parse
+import json
 
 outfile = open('postb', 'w')
-params = ({ 'vote': 'b' })
-encoded = urllib.parse.urlencode(params)
+params = ({ 'vote': 'Dogs' })
+encoded = json.dumps(params)
 outfile.write(encoded)
 outfile.close()
 outfile = open('posta', 'w')
-params = ({ 'vote': 'a' })
-encoded = urllib.parse.urlencode(params)
+params = ({ 'vote': 'Cats' })
+encoded = json.dumps(params)
 outfile.write(encoded)
 outfile.close()
